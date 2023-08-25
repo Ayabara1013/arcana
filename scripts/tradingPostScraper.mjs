e334892e8090e4e70a30fba0009359e9995ad943
Jeremy Allan <jallan8210@gmail.com>
2023-08-25 16:36:17 -0400

    successfully saved name and caption as a string

--- scripts/tradingPostScraper.mjs
+++ scripts/tradingPostScraper.mjs
@@ -1,33 +1,30 @@
-// const fs = require('fs');
-// const path = require('path');
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-
-// DO NOT INCLUDE THIS ANYWHERE IN THE ACTUAL CODE SO .MJS DOESNT CAUSE ISSUES WHEN BUILDING!
-
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import cheerio from 'cheerio';
-
 import inquirer from 'inquirer';
+
+const filename = 'tradingPostScraperResults.json';
 
 async function scrapeTradingPost() {
   const { url, year, month, outputFile } = await inquirer.prompt([
     {
       type: 'input',
       name: 'url',
-      message: 'Enter the URL to scrape: '
+      message: 'Enter the URL to scrape: ',
+      default: 'https://worldofwarcraft.blizzard.com/en-us/news/23985778/wrap-up-the-summer-with-the-august-trading-post'
     },
     {
       type: 'input',
       name: 'year',
-      message: 'Enter the year: '
+      message: 'Enter the year: ',
+      default: '2023'
     },
     {
       type: 'input',
       name: 'month',
-      message: 'Enter the month: '
+      message: 'Enter the month: ',
+      default: 'august'
     },
     {
       type: 'input',
@@ -42,25 +39,74 @@ async function scrapeTradingPost() {
       const html = response.data;
       const $ = cheerio.load(html);
 
-      const items = [];
+      const items = {};
+      
 
       // Assuming each <figure> element contains an image and caption
-      $('aside.gallery figure').each((index, element) => {
-        const imgSrc = $(element).find('img').attr('src');
-        const caption = $(element).find('figcaption').text();
+      $('figure').each((index, element) => {
+        // check for image
+        const img = $(element).find('img');
+        if (!img.length) {
+          return; // Skip this element if there is no img
+        }
+        const imgSrc = img.attr('src');
 
-        items.push({ imgSrc, caption });
+        // check for figcaption
+        const figcaption = $(element).find('figcaption');
+        if (!figcaption) {
+          return; // Skip this element if there is no figcaption
+        }
+
+        // pull the caption text
+        const caption = figcaption.text().trim();
+
+        // get the name
+        const name = caption.split('\n')[0].trim();
+
+        // get the tags
+
+        // get the price
+      
+        const item = {
+          "name": name,
+          // "price": cost,
+          "image": imgSrc,
+          // "image": imgSrc,
+          // "tags": [tags],
+          // "currentAvailability": true,
+          // "pastAvailability": [null]
+          "caption": caption,
+        };
+
+        items[name] = item;
+      
+        console.log(`Item ${index}:`, item.name);
       });
 
-      // Now you have the scraped data in the 'items' array
+      // Now you have the scraped data in the 'items' object
       // You can do further processing or save it to your object
 
       console.log(items); // Just for demonstration
 
-      // Save the data to a file
-      const data = JSON.stringify(items, null, 2);
-      const filePath = path.resolve(outputFile);
-      fs.writeFileSync(filePath, data);
+      // Read the existing data from the JSON file
+      let existingData = {};
+      if (fs.existsSync(filename)) {
+        const fileContents = fs.readFileSync(filename, 'utf8');
+        existingData = JSON.parse(fileContents);
+      }
+
+      // Add new items to the existing data
+      for (const itemName in items) {
+        if (existingData[itemName]) {
+          console.log(`Item ${itemName} already exists in the JSON file`);
+          continue;
+        }
+        existingData[itemName] = items[itemName];
+      }
+
+      // Write the updated data to the JSON file
+      const data = JSON.stringify(existingData, null, 2);
+      fs.writeFileSync(filename, data);
       console.log(`Data saved to ${filePath}`);
     })
     .catch(error => {
@@ -69,3 +115,29 @@ async function scrapeTradingPost() {
 }
 
 scrapeTradingPost();
+
+/**
+ * example html
+ * 
+ * <aside class="gallery">
+<figure><img alt="" src="https://bnetcmsus-a.akamaihd.net/cms/content_entry_media/39N6E7K1HH2X1690524390194.png">
+<figcaption>
+<p style="text-align: center;"><u><strong>City Guard Heater Shield</strong></u><br>
+<strong>Item Type:</strong>&nbsp;Shield<br>
+<strong>Cost:</strong>&nbsp;50 Trader's Tender</p>
+
+<p align="center" style="text-align:center"></p>
+</figcaption>
+</figure>
+
+<figure><img alt="" src="https://bnetcmsus-a.akamaihd.net/cms/content_entry_media/BTG222U1JGJQ1690524392863.png">
+<figcaption>
+<p style="text-align: center;"><u><strong>Grunt’s Buckler</strong></u><br>
+<strong>Item Type:</strong>&nbsp;Sheild<br>
+<strong>Cost:</strong>&nbsp;50 Trader's Tender</p>
+
+<p align="center" style="text-align:center"></p>
+</figcaption>
+</figure>
+</aside>
+ */
