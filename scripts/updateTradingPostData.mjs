1c49260ff7ba63d4cea332873ecc18ef0f20e1d3
Jeremy Allan <jallan8210@gmail.com>
2023-09-13 16:51:04 -0400

    I finally fixed the file, and made the script fix things properly in the future!
    - cant wait till I have too many items to fix in the future without lagging my pc like crazy hahaha

--- scripts/updateTradingPostData.mjs
+++ scripts/updateTradingPostData.mjs
@@ -1,36 +1,32 @@
-// import { each } from 'cheerio/lib/api/traversing';
+import { count } from 'console';
 import fs from 'fs';
 
 // const filename = 'tradingPostScraperResults.json';
 const filename = '../src/assets/data/tradingPostScraperResults.json';
 const itemRecords = '../src/assets/data/itemRecords.json';
+const scanResults = '../src/assets/data/scanResults.json';
+const testFile = '../src/assets/data/testFile.json';
+const backup = '../src/assets/data/scraperResultsBackup.json';
 
 // create an object to manipulate the data inside
 let existingData = {};
 let existingItems = {};
+let resultsFromScan = {};
 
 // Read the existing data from the JSON file
 if (fs.existsSync(filename)) {
   const fileContents = fs.readFileSync(filename, 'utf8');
   existingData = JSON.parse(fileContents);
-  // console.log(existingData);
-  // console.log(`scraper data file exists`)
-
-  fixTradingPostStrings();
-  printExistingData();
 }
-// // not really needed! i think lol
-// if (fs.existsSync(itemRecords)) {
-//   const fileContents = fs.readFileSync(filename, 'utf8');
-//   existingData = JSON.parse(fileContents);
-// }
 
 function printExistingData() {
+  // console.log(existingData);
   for (const year in existingData) {
     for (const month in existingData[year]) {
-      for (const item in existingData[year][month]) {
-        console.log(existingData[year][month][item]);
-      }
+      console.log(existingData[year][month]);
+      // for (const item in existingData[year][month]) {
+      //   console.log(item);
+      // }
     }
   }
 }
@@ -42,67 +38,154 @@ function fixedJSONString(string) {
   return newString;
 }
 
+function getTypeOf(data) {
+  let type = typeof data;
+  if (type === 'object') {
+    if (Array.isArray(data)) {
+      type = 'array';
+      if (data.length > 1) {
+        type = 'array, nested';
+      }
+    }
+  }
+  return type;
+}
+
+function scanTpResuts() {
+  for (const year in existingData) {
+    resultsFromScan[year] = {};
+    for (const month in existingData[year]) {
+      resultsFromScan[year][month] = {};
+      for (const item in existingData[year][month]) {
+        resultsFromScan[year][month][item] = {};
+
+        for (const property in existingData[year][month][item]) {
+          let propertyData = existingData[year][month][item][property];
+          console.log(`------`);
+          console.log(property);
+
+          let type = typeof propertyData;
+          if (type === 'object') {
+            if (Array.isArray(propertyData)) {
+              type = 'array';
+              if (propertyData.length > 1) {
+                type = 'array, nested';
+              }
+            }
+          }
+
+          resultsFromScan[year][month][item][property] = type;
+          console.log(resultsFromScan[year][month][item][property]);
+        }
+      }
+    }
+  }
+  // I want this to write a new file with the results of the scan
+  fs.writeFileSync(scanResults, JSON.stringify(resultsFromScan, null, 2));
+}
+
 function fixTradingPostStrings() {
+  let tnew = 'new item ->';
   for (const year in existingData) {
     for (const month in existingData[year]) {
       for (const item in existingData[year][month]) {
-        console.log('-------------------')
-        // let newItemName = fixedJSONString(item);
-        // if (item !== newItemName) {
-        //   console.log(`${item} does not match ${newItemName}`);
-
-        //   // add a new item with the new data
-        //   // existingData[year][month][newItemName] = existingData[year][month][item];
-
-          
-        //   // delete existingData[year][month][item];
-        //   // console.log(existingData[year][month]);
-        // }
-
-        // for (const property in existingData[year][month][item]) { 
-        //   // existingData[year][month][newItemName][property] = existingData[year][month][item][property];
-        //   console.log(existingData[year][month][newItemName][property]);
-        //   console.log(existingData[year][month][item][property]);
-        // }
-        
-        // console.log(`WRONG! ${item}`);
+        console.log('---------------------------------------------------------')
         
         if (item.includes('’') || item.includes('–')) {
           console.log(`WRONG! ${item}`);
         }
 
+        // diplay the original item
+        console.log(item, existingData[year][month][item]);
+
         let newItemName = fixedJSONString(item);
+        existingData[year][month][newItemName] = existingData[year][month][item];
 
-        // console.log(existingData[year][month][item]);
-
-        console.log(newItemName);
-
+        // new item now has the fixed name
         let newItem = existingData[year][month][newItemName];
+        console.log(`new item -> ${newItemName}`, newItem);
 
-        console.log(newItem);
-
-        for (const property in existingData[year][month][item]) {
-          existingData[year][month][newItemName][property] = existingData[year][month][item][property];
+        for (const property in newItem) {
+          // item prints the values correctly, now I need to change them
+          if (getTypeOf(newItem[property]) === 'string') {
+            let newString = fixedJSONString(newItem[property]);
+            // console.log(`new string -> ${newString}`);
+            newItem[property] = newString;
+          }
         }
 
-        delete existingData[year][month][item];
+        console.log(`new item -> ${newItemName}`, newItem);
+        // console.log(`old item -> ${item}`, existingData[year][month][item]);
+      }
+      console.log(existingData[year][month])
+    }
+  }
+  fs.writeFileSync(testFile, JSON.stringify(existingData, null, 2));
+}
 
-        // console.log(existingData[year][month][newItemName]);
-
-        // console.log(existingData[year][month][newItemName]);
-
-        // for (const property in existingData[year][month][item]) {
-        //   // existingData[year][month][item][property] = fixedJSONString(existingData[year][month][item][property]);
-        //   if (property === "name" || property === "caption") {
-        //     if (existingData[year][month][item][property].includes('’')) {
-        //       // console.log(`${item} ${property} includes bad character`);
-        //       // existingData[year][month][item][property] = fixedJSONString(existingData[year][month][item][property]);
-        //     }
-        //   }
-        // }
+function countEntries(data) {
+  let count = 0;
+  for (const year in data) {
+    for (const month in data[year]) {
+      for (const item in data[year][month]) {
+        count++;
       }
     }
   }
+  console.log(count);
+}
+
+function fixAll() {
+  let newData = {}; // this is the new data that will be written to the file
+  let arrow = '->';
+  let tNew = 'new item';
+  let tOld = 'old item';
+
+  // proof that some items dont have the data
+  console.log(existingData['2023']['september']['Ensemble: Swashbuckling Bucaneer’s Slops']);
+
+  for (const year in existingData) {
+    newData[year] = {};
+    for (const month in existingData[year]) {
+      newData[year][month] = {};
+      for (const item in existingData[year][month]) {
+        let newName = fixedJSONString(item);
+        let newItem = newData[year][month][newName] = {};
+        Object.assign(newItem, existingData[year][month][item]);
+
+         // check if lastAvailable exists
+         if (!newItem.lastAvailable) {
+          console.log(newName);
+          console.log('missing! >>>');
+          newItem.lastAvailable = [[year, month]];
+          console.log(newName, newItem.lastAvailable);
+        }
+        
+        // replace the old strings with the new fixed strings
+        for (const property in newItem) {
+          if (getTypeOf(newItem[property]) === 'string') {
+            let newString = fixedJSONString(newItem[property]);
+            newItem[property] = newString;
+          }
+        }
+
+        console.log(`---------------------------------------------------------`);
+        console.log(`old item ::`);
+        console.log(item, arrow, existingData[year][month][item]); // ok so the item is still there
+
+        console.log(`new item ::`);
+        console.log(newName, arrow, newItem);
+      } // item
+    } // month
+  } // year
+
+  // // proof that the items now have the data
+  // console.log(newData['2023']['september']);
+  countEntries(existingData);
+  countEntries(newData);
+  fs.writeFileSync(backup, JSON.stringify(newData, null, 2));
+  fs.writeFileSync(testFile, JSON.stringify(newData, null, 2));
+  fs.writeFileSync(filename, JSON.stringify(newData, null, 2));
 }
 
 
@@ -212,12 +295,12 @@ function finalChecks() {
   for (const item in existingItems) {
     if (item.length > 1) console.log(item)
   }
-  // console.log(existingData["2023"]["february"]);
-// console.log(existingItems);
 }
 
 // updateTradingPostData();
 // fixTradingPostStrings();
 // recordItems();
 // updateScraperResults();
+fixAll();
+countEntries(existingData);
 finalChecks();
