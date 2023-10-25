f5852e7682b9c9c3d6bb2e7708b104aa8c2bca1b
Jeremy Allan <jallan8210@gmail.com>
2023-10-25 15:08:24 -0400

    the roll function seems to be complete!

--- src/characterSheet/dice roller/rolls.js
+++ src/characterSheet/dice roller/rolls.js
@@ -1,10 +1,13 @@
 // import { parse } from 'yargs';
 
+import { resolveMotionValue } from 'framer-motion';
 
 
-export class Results {
-  constructor(sum, rolls, mods) {
+
+export class Result {
+  constructor(sum, totals, rolls, mods) {
     this.sum = 0;
+    this.totals = []
     this.rolls = {
       d4: [],
       d6: [],
@@ -14,16 +17,16 @@ export class Results {
       d20: [],
       d100: [],
     };
-    this.mods = {};
+    this.mods = [];
   }
 }
 
 export class Roll {
   constructor(operation, quantity, die, type) {
     this.operation = this.setOperation(operation);
-    this.quantity = quantity ?? 0;
-    this.die = die ?? 0;
-    this.type = type ?? null;
+    this.quantity = parseInt(quantity) ?? 0;
+    this.die = parseInt(die) ?? 0;
+    this.type = this.setType(type);
   }
 
   setOperation(operation) {
@@ -40,13 +43,32 @@ export class Roll {
       return 'add';
     }
   }
+
+  setType(type) {
+    if (type.length <= 1 && type[0] === '') {
+      return [false]; // i am going to leave this as an array in case there would be any weird errors calling it later, ie type[0] === false? and so on 
+    }
+    return type;
+  }
+}                                         
+
+class RollResult extends Roll { // can this be just made its own separate class? is there any benefit in linking it to Roll?
+  constructor(operation, die, type) {
+    super(operation, 1, die, type);
+    this.result = this.getResult();
+    // delete this.setType;
+  }
+
+  getResult() {
+    return this.operation === 'add' ? rollDie(this.die) : this.operation === 'subtract' ? -rollDie(this.die) : `ERROR`;
+  }
 }
 
 export class Modifier {
-  constructor(operation, value, source, type) {
+  constructor(operation, value, type, source) {
     this.operation = this.setOperation(operation); // operation is either 'add' or 'subtract'
-    this.value = value ?? 0;      // value is the number of the modifier, like 2 or 5 
-    this.type = type ?? null;     // type is either 'flat' or 'multiplier' // OR // type will be something like proficiency, fire, etc. // should this be a tags array?
+    this.value = parseInt(value) ?? 0;      // value is the number of the modifier, like 2 or 5 
+    this.type = this.setType(type);     // type is either 'flat' or 'multiplier' // OR // type will be something like proficiency, fire, etc. // should this be a tags array?
     this.source = source ?? null; // source is the name of the modifier, like 'strength' or 'proficiency'
   }
 
@@ -63,6 +85,13 @@ export class Modifier {
     else {
       return 'add';
     }
+  }
+
+  setType(type) {
+    if (type.length <= 1 && type[0] === '') {
+      return [false]; // i am going to leave this as an array in case there would be any weird errors calling it later, ie type[0] === false? and so on 
+    }
+    return type;
   }
 }
 
@@ -116,49 +145,75 @@ export function parseRolls(command) {
   // add the modifiers to the results
   for (let i = 0; i < modMatches.length; i++) {
     const match = modMatches[i].match(/([+\-!](\S+)?)\s?(?:(?!\d+d\d+)[\w\s])+/);
-    // console.log(match[0]);
 
     const operation = match[1][0];
     const value = match[0].slice(1).trim().match(/(\d+)/)[0];
     const type = match[0].replace(/[^\w\s]|[\d]/g, '').trim().split(' ');
     // const result = new Modifier(operation, value, type);
 
-    // console.log(operation);
-    // console.log(value);
-    // console.log(type);
-
     results.mods.push(new Modifier(operation, value, type));
   }
-
-  // console.log(results);
 
   return results;
 }
 
-// parseRolls();
-
 export function roll(rolls) {
-  const results = new Results();
-  console.log(rolls);
-  console.log(rolls.rolls);
-  console.log(rolls.mods);
+  const result = new Result();
+  console.clear();
+  // console.log(rolls);
 
+  // roll the dice
   if (rolls.rolls) {
     console.log('--- rolling dice ---');
 
     for (let i = 0; i < rolls.rolls.length; i++) {
-      let rr = rolls.rolls[i];
-      let string = `${rr.operation === 'add' ? '+' : rr.operation === 'subtract' ? '-' : ''} ${rr.quantity}d${rr.die} ${rr.type[0] !== '' ? `[ ${rr.type.join(', ')} ]` : `[ untyped ]`}`;
-      console.log(string);
+      const rr = rolls.rolls[i];
+
+      // displayRollsGroup(rr);
+
+      for (let dice = 0; dice < rr.quantity; dice++) {
+        const nr = new RollResult(rr.operation, rr.die, rr.type);
+        console.log(nr.result, nr.die, nr.type);
+
+        result.rolls[`d${rr.die}`].push(nr);
+        result.sum += nr.result;
+        result.totals.push([nr.result, nr.die, nr.type])
+      }
     }
   }
 
+  // apply modifiers
   if (rolls.mods) {
     console.log('--- applying modifiers ---');
     
+    for (let mod = 0; mod < rolls.mods.length; mod++) {
+      const mm = rolls.mods[mod];
+      // console.log(mm);
+      console.log(mm.operation === 'subtract' ? -mm.value : mm.value, mm.type)
+
+      result.mods.push(mm);
+      result.sum += mm.value;
+      result.totals.push([mm.value, mm.type]);
+    }
   }
+
+  console.log(result);
 }
 
 export function displayRollResults() {
   return `--- displaying roll results ---`;
+}
+
+const displayRollsGroup = (rr) =>  {
+  let rollString = `${rr.operation === 'add' ? '+' : rr.operation === 'subtract' ? '-' : ''} ${rr.quantity}d${rr.die}`;
+
+  if (rr.type.includes(false)) {
+    rollString += ' [ untyped ]';
+  }
+  else {
+    rollString += ` ${rr.type[0] !== '' ? `[ ${rr.type.join(', ')} ]` : `[untyped]`}`;
+  }
+
+  console.log(rollString);
+  console.log(rr);
 }
